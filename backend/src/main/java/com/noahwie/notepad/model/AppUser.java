2cca222b306c2761110ff79fa57640cc7421e641
Noah WIeschmann <124290589+noahwie@users.noreply.github.com>
2026-01-21 10:07:00 +0100

    feat: Add AppUser entity with roles

---
+++ backend/src/main/java/com/noahwie/notepad/model/AppUser.java
@@ -0,0 +1,40 @@
+package com.noahwie.notepad.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.management.relation.Role;
+
+@Entity
+@Table(name = "app_users")
+@Getter
+@Setter
+public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public AppUser() {}
+
+    public AppUser(String username, String email, String password, Role role) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+}
